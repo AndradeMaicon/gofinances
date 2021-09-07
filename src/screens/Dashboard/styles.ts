b3d574eb7395b6dfb151e7d405e8d706e2d26ca3
Maicon <andrade.maicon.pr@gmail.com>
2021-09-07 15:36:14 -0300

    Creating transaction list

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { DataListProps } from '.';
 
 export const Container = styled.View`
   flex: 1;
@@ -74,3 +77,26 @@ export const HighlightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transaction = styled.View`
+  flex: 1;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(16)}px;
+`;
+
+export const Title = styled.Text`
+  color: ${({theme}) => theme.colors.text_dark};
+
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}) => theme.fonts.regular};
+
+  margin-bottom: 16px;
+`;
+
+export const TransactionsList = styled(
+    FlatList as new () => FlatList<DataListProps>
+  ).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { paddingBottom: getBottomSpace() }
+})``;
